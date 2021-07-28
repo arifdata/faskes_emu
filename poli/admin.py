8d9dc2f2b942880841df0f6547d44c4edeeccefd
Arif Mulpratama <arifmul@gmail.com>
2021-07-28 19:04:25 +0700

    hilangkan django-jazzmin

--- poli/admin.py
+++ poli/admin.py
@@ -5,18 +5,22 @@ from apotek.models import Resep
 # Register your models here.
 @admin.register(DataPeresep)
 class DataPeresepAdmin(admin.ModelAdmin):
-    def log_addition(self, *args):
-        return
+	def has_module_permission(self, request):
+		return {}
+	def log_addition(self, *args):
+		return
 
 @admin.register(Diagnosa)
 class DiagnosaAdmin(admin.ModelAdmin):
-    def log_addition(self, *args):
-    	return
+	def has_module_permission(self, request):
+		return {}
+	def log_addition(self, *args):
+		return
 
 class ResepInline(admin.TabularInline):
-    model = Resep
-    autocomplete_fields = ['nama_obat']
-    extra = 5
+	model = Resep
+	autocomplete_fields = ['nama_obat']
+	extra = 5
 
 @admin.register(DataKunjungan)
 class DataKunjunganAdmin(admin.ModelAdmin):
@@ -24,3 +28,4 @@ class DataKunjunganAdmin(admin.ModelAdmin):
 	autocomplete_fields = ['nama_pasien']
 	list_display = ('nama_pasien', 'tgl_kunjungan', 'no_resep')
 	list_per_page = 20
+	filter_horizontal = ('diagnosa',)
