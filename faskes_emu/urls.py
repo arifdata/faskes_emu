7006e3e29fc34a1ef0a25b29b5e64bd52d547b5c
Arif Mulpratama <arifmul@gmail.com>
2021-07-30 16:17:24 +0700

    simple root level page

--- faskes_emu/urls.py
+++ faskes_emu/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from pendaftaran.views import index_page
 
 urlpatterns = [
     path('app/', admin.site.urls),
+    path('', index_page),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pendaftaran/views.py
+++ pendaftaran/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index_page(request):
+    html = """
+    <h1>Welcome</h1>
+    <a href='app/'>Login Ke App</a>
+    """
+    return HttpResponse(html)
