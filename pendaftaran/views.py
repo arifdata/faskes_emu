ce043c19d7ad2507e4dd7bca5614fc1971b056dd
Arif Mulpratama <arifmul@gmail.com>
2021-07-30 17:06:40 +0700

    using template

--- pendaftaran/views.py
+++ pendaftaran/views.py
@@ -3,8 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index_page(request):
-    html = """
-    <h1>Welcome</h1>
-    <a href='app/'>Login Ke App</a>
-    """
-    return HttpResponse(html)
+    context = {'nama': 'User'}
+    return render(request, 'pendaftaran/index.html', context)
