61a01dfa5f9f392c617837da585f539b6281cdce
Arif Mulpratama <arifmul@gmail.com>
2021-07-30 22:16:45 +0700

    added frappe chart library

--- pendaftaran/views.py
+++ pendaftaran/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index_page(request):
-    context = {'nama': 'User'}
+    context = {'nama': 'User', 'data': [25, 40, 30, 35, 8, 52, 17, -4]}
     return render(request, 'pendaftaran/index.html', context)
