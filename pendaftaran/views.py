bf0c11eeeed9147ee89b807c75facf3b017e4274
Arif Mulpratama <arifmul@gmail.com>
2021-07-31 12:19:02 +0700

    implemented penyakit dalam sebulan di root page

--- pendaftaran/views.py
+++ pendaftaran/views.py
@@ -9,21 +9,35 @@ def index_page(request):
 
     # get tanggal sekarang
     now = datetime.datetime.now()
+    raw_data_penyakit = {}
 
     #query data dari awal bulan sekarang sampai sekarang
     query = DataKunjungan.objects.filter(tgl_kunjungan__gte="{}-{}-1".format(now.year, now.month), tgl_kunjungan__lte="{}-{}-{}".format(now.year, now.month, now.day))
 
-    #dict utk menampung data sementara
-    raw_data = {}
-
-    # memasukkan tanggal beserta jumlah kunjungannya respectively ke dict raw_data
+    # memasukkan diagnosa ke raw_data_penyakit dan menghitung akumulasinya
     for data in query:
+        for diag in data.diagnosa.values('diagnosa'):
+            if diag['diagnosa'] not in raw_data_penyakit:
+                raw_data_penyakit[diag['diagnosa']] = 1
+            else:
+                raw_data_penyakit[diag['diagnosa']] += 1
+
+    #dict utk menampung data sementara
+    raw_data_kunjungan = {}
+    
+    # memasukkan tanggal beserta jumlah kunjungannya respectively ke dict raw_data_kunjungan
+    for data in query:
+        #print(data.diagnosa.values('diagnosa'))
         b = DataKunjungan.objects.filter(tgl_kunjungan=data.tgl_kunjungan)
-        raw_data[data.tgl_kunjungan.strftime('%d-%m')] = len(b)
+        raw_data_kunjungan[data.tgl_kunjungan.strftime('%d-%m')] = len(b)
 
     # mengurutkan data sesuai urutan tanggal
-    cleaned_data = OrderedDict(sorted(raw_data.items()))
+    cleaned_data_kunjungan = OrderedDict(sorted(raw_data_kunjungan.items()))
 
-    print(cleaned_data)
-    context = {'nama': 'Power User', 'labels': list(cleaned_data.keys()), 'data': list(cleaned_data.values())}
+    # mengurutkan data sesuai urutan tanggal
+    cleaned_data_penyakit = OrderedDict(sorted(raw_data_penyakit.items()))
+    
+    print(cleaned_data_penyakit)
+
+    context = {'nama': 'Power User', 'labels_penyakit': list(cleaned_data_penyakit.keys()), 'data_penyakit': list(cleaned_data_penyakit.values()), 'labels_kunjungan': list(cleaned_data_kunjungan.keys()), 'data_kunjungan': list(cleaned_data_kunjungan.values())}
     return render(request, 'pendaftaran/index.html', context)
