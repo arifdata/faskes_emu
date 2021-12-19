06ee6c6e5e8af2339c59db658d1c2f3b2a1dc4c0
Arif Mulpratama <arifmul@gmail.com>
2021-12-19 08:26:41 +0700

    trying login required decorator

--- laporan/forms.py
+++ laporan/forms.py
@@ -5,8 +5,6 @@ class TglForm(forms.Form):
     tanggal1 = forms.CharField(label="Tanggal Awal", widget=forms.widgets.DateTimeInput(attrs={"type": "date"}))
     tanggal2 = forms.CharField(label="Tanggal Akhir", widget=forms.widgets.DateTimeInput(attrs={"type": "date"}))
 
-class TglChoiceForm(forms.Form):
+class TglChoiceForm(TglForm):
     CHOICES = [('apotek', 'Apotek'), ('gudang', 'Gudang')]
-    tanggal1 = forms.CharField(label="Tanggal Awal", widget=forms.widgets.DateTimeInput(attrs={"type": "date"}))
-    tanggal2 = forms.CharField(label="Tanggal Akhir", widget=forms.widgets.DateTimeInput(attrs={"type": "date"}))
     pilihan = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
