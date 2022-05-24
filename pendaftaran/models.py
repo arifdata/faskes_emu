e0c0dd2447d4890c69d35402c410593393c10401
Arif M <arifdata7@gmail.com>
2022-05-24 23:01:02 +0700

    remove default value pendaftaran model

--- pendaftaran/models.py
+++ pendaftaran/models.py
@@ -3,11 +3,11 @@ import datetime
 
 # Create your models here.
 class DataPasien(models.Model):
-    no_kartu = models.CharField(max_length=20, help_text="Masukkan no BPJS / KTP", default="000", verbose_name="Nomor Kartu")
+    no_kartu = models.CharField(max_length=20, help_text="Masukkan no BPJS / KTP", verbose_name="Nomor Kartu")
     nama_pasien = models.CharField(max_length=30, help_text="Masukkan nama", verbose_name="Nama Pasien")
     alamat = models.CharField(max_length=50, help_text="Masukkan alamat", blank=True, verbose_name="Alamat")
     usia = models.DateField(help_text="dd-mm-yy", verbose_name="Tanggal Lahir", blank=True)
-    no_hp = models.CharField(max_length=15, help_text="masukkan dengan format +628XXXXXXXXXX", verbose_name="No Telepon", blank=True, default="00000")
+    no_hp = models.CharField(max_length=15, help_text="masukkan dengan format +628XXXXXXXXXX", verbose_name="No Telepon", blank=True)
     
     def __str__(self):
         u = self.umur()
