21c7638f305b71f9aa183c2a537fae89535b4b33
Arif M <arifdata7@gmail.com>
2022-07-03 14:22:37 +0700

    berhasil menjembatani excel penerimaan ke faskes via rest. tapi msh masalah bila tgl_kadaluarsa kosong

--- otomatisasi/restauto/cobagorm.go
+++ otomatisasi/restauto/cobagorm.go
@@ -1,25 +1,35 @@
 package restauto
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/xuri/excelize/v2"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
-type ApotekDataobat struct {
-	ID           int32  `gorm:"primary_key;AUTO_INCREMENT;column:id;type:integer;"`
-	NamaObat     string `gorm:"column:nama_obat;type:varchar;size:200;"`
-	Satuan       string `gorm:"column:satuan;type:varchar;size:3;"`
-	IsAb         string
-	IsOkt        string
-	IsNonGenerik string
-	IsAlkes      string
-	IsJkn        string
+func HurufConvert(s string) int {
+	huruf, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return huruf
 }
 
-func (ApotekDataobat) TableName() string {
-	return "apotek_dataobat"
+func TanggalFixer(s string) string {
+	if s == "-" {
+		return "2030-10-12"
+	} else {
+		tgl := strings.Split(s, "-")
+		bener := fmt.Sprintf("%s-%s-%s", tgl[2], tgl[1], tgl[0])
+		return bener
+	}
+
 }
 
 func CobaGorm() {
@@ -36,23 +46,32 @@ func CobaGorm() {
 		return
 	}
 	defer func() {
-		// Close the spreadsheet.
+		// tutup file excel di akhir scope.
 		if err := f.Close(); err != nil {
 			fmt.Println(err)
 		}
 	}()
 
-	// Get all the rows in the Sheet1.
+	// ambil semua baris di Sheet1.
 	rows, err := f.GetRows("Sheet1")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	record_count := 0
+	tglterima := ""
+	sumberterima := ""
+
 	for index, row := range rows {
-		// indeks 0 adalah header bukan daftar obat, maka kita abaikan
-		if index > 0 {
+		if index == 0 {
+			tglterima = row[1]
+			sumberterima = row[3]
+		} else if index > 3 {
+			record_count += 1
 			var obat ApotekDataobat
 			result := db.First(&obat, "nama_obat = ?", row[1])
+			//fmt.Println(obat.NamaObat)
 
 			// handle bila obat belum ada di dalam database
 			if result.Error != nil {
@@ -91,9 +110,48 @@ func CobaGorm() {
 
 				//masukkan record baru ke database
 				db.Create(&bmhp_baru)
-
 			}
 		}
 	}
+
+	var sumber ApotekSumberterima
+	db.First(&sumber, "nama = ?", sumberterima)
+	data := &BukuPenerimaan{}
+	data.PenerimaanSet = make([]Penerimaan, record_count)
+	data.Sumber = sumber.ID
+	data.TanggalTerima = tglterima
+	data.Notes = ""
+	penerimaan_count := 0
+
+	for index, row := range rows {
+		if index > 3 {
+			var obat ApotekDataobat
+			db.First(&obat, "nama_obat = ?", row[1])
+			data.PenerimaanSet[penerimaan_count] = Penerimaan{
+				NamaBarang:    obat.ID,
+				Jumlah:        HurufConvert(row[4]),
+				TglKadaluarsa: TanggalFixer(row[9]),
+				NoBatch:       row[8],
+			}
+			penerimaan_count += 1
+		}
+
+	}
+
+	a, _ := json.Marshal(data)
+	req, err := http.NewRequest("POST", "http://localhost:8000/api/bukupenerimaan/", bytes.NewBuffer(a))
+	req.SetBasicAuth("admin", "puskesmas")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
 
 }
--- otomatisasi/restauto/otopenerimaan.go
+++ otomatisasi/restauto/otopenerimaan.go
@@ -8,6 +8,30 @@ import (
 	"net/http"
 )
 
+type ApotekDataobat struct {
+	ID           int    `gorm:"primary_key;AUTO_INCREMENT;column:id;type:integer;"`
+	NamaObat     string `gorm:"column:nama_obat;type:varchar;size:200;"`
+	Satuan       string `gorm:"column:satuan;type:varchar;size:3;"`
+	IsAb         string
+	IsOkt        string
+	IsNonGenerik string
+	IsAlkes      string
+	IsJkn        string
+}
+
+func (ApotekDataobat) TableName() string {
+	return "apotek_dataobat"
+}
+
+type ApotekSumberterima struct {
+	ID   int    `gorm:"primary_key;AUTO_INCREMENT;column:id;type:integer;"`
+	Nama string `gorm:"column:nama;type:varchar;size:20;"`
+}
+
+func (ApotekSumberterima) TableName() string {
+	return "apotek_sumberterima"
+}
+
 type Penerimaan struct {
 	NamaBarang    int    `json:"nama_barang"`
 	Jumlah        int    `json:"jumlah"`
